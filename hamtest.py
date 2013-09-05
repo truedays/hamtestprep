a7ea913a49a253cd3c7218053e5a413bf016bdca
Ray <ray@truedays.org>
2013-09-04 23:03:23 -0500

    Update hamtest.py
    fixed Question7 bug, by looking for \n and -1'ing the questionID assumed location relative to the question break '~~\n'

--- hamtest.py
+++ hamtest.py
@@ -6,11 +6,21 @@ f = open('dos2unix.out', 'r')
 file = f.readlines()
 questionid = ["",]
 qnum = 0
+
 for n in range(0,2816):
-        if file[n] == "~~\n":
-                questionid[qnum:] = [file[n-6][0:5]]
-                qnum = qnum + 1
-                print questionid[qnum -1]
+  if file[n] == "~~\n":
+    #if file[n-6] == "":
+    #  questionid[qnum:] = [file[n-5][0:5]
+    #  qnum = qnum + 1
+    #  print questionid[qnum -1]
+    #  continue
+    questionid[qnum:] = [file[n-6][0:5]]
+#    print questionid[qnum:]
+#    print questionid[qnum]
+    if questionid[qnum] == '\n':  questionid[qnum:] = [file[n-5][0:5]]
+# TODO: add QuestionCount, so I know how many questions to extract 
+    qnum += 1
+    print questionid[qnum -1]
 
 #import re
 #re.findall(r'\AT[1-9][A-Z][0-9][0-9]',file[22])
