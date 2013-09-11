ad612fe343cd8d450eb8e576315e9a3bcf5530cf
truedays <ray@eboundhost.com>
2013-09-10 20:32:50 -0500

    nested lists

--- hamtestprep.py
+++ hamtestprep.py
@@ -2,29 +2,22 @@
 #
 # this will be a complete rewrite of hamtestprep in pythion 2.x
 
-import re
 f = open('dos2unix.out', 'r')
 file = f.readlines()
-questionid = []
-answerkey= []
+question = []
 qnum = 0
 
 for n in range(0,100):
 #for n in range(0,2816):
   if file[n] == "~~\n":
-    questionid[qnum:] = [file[n-6][0:5]]
-    answerkey[qnum:] = file[n-6][file[n-6].find("(")+1]
-    if questionid[qnum] == '\n':
-        questionid[qnum:] = [file[n-5][0:5]]
-        answerkey[qnum:] = file[n-5][file[n-5].find("(")+1]
-# TODO: add QuestionCount, so I know how many questions to extract 
+    if file[n-6][0:5] == '\n':
+      question.append([file[n-5][0:5]])
+      question[qnum].append(file[n-5][7:8])
+    else:
+      question.append([file[n-6][0:5]])
+      question[qnum].append(file[n-6][7:8])
+    print question[qnum]
     qnum += 1
-    print questionid[qnum -1]
-    print questionid[qnum -1:]
-    print answerkey[qnum -1]
-print questionid[6]
-print questionid
-questionid[len(questionid):] = ["thisworks"]
-print questionid
-#import re
-#re.findall(r'\AT[1-9][A-Z][0-9][0-9]',file[22])
+print ""
+print question
+print "The End"
