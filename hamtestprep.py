2f185ccea4bc4b8865c93abcc312a611cbfc2846
truedays <ray@eboundhost.com>
2013-09-10 20:46:44 -0500

    first non-iteractive question asked

--- hamtestprep.py
+++ hamtestprep.py
@@ -7,8 +7,7 @@ file = f.readlines()
 question = []
 qnum = 0
 
-for n in range(0,100):
-#for n in range(0,2816):
+for n in range(0,2816):
   if file[n] == "~~\n":
     if file[n-6][0:5] == '\n':
       question.append([file[n-5][0:5]])
@@ -20,8 +19,10 @@ for n in range(0,100):
       question[qnum].append(file[n-6][7:8])
       question[qnum].append(file[n-5])
       question[qnum].append([file[n-4], file[n-3], file[n-2], file[n-1]])
-    print question[qnum]
+    #print question[qnum]
     qnum += 1
-print ""
-print question
-print "The End"
+
+# for Q in range(qnum)
+print question[0][2],
+for n in question[0][3]:
+  print n,
